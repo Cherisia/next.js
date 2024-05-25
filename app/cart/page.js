b9e751c3079c1882f598aa2121c339c7f76f7006
Cherisia <yhcyoon@kakao.com>
2024-05-25 16:31:18 +0900

    컴포넌트, import

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,6 +1,12 @@
+import {age, name} from './data.js';
+import Hello from "./hello";
+
 export default function Cart() {
+    console.log('age : ' + age);
+    console.log('name : ' + name);
     return (
         <div>
+            <Hello/>
             <h4 className="title">Cart</h4>
             <CartItem/>
         </div>
@@ -9,10 +15,17 @@ export default function Cart() {
 
 function CartItem() {
     return (
-        <div className="cart-item">
-            <p>상품명</p>
-            <p>$40</p>
-            <p>1개</p>
+        <div>
+            <div className="cart-item">
+                <p>상품명</p>
+                <p>$40</p>
+                <p>1개</p>
+            </div>
+            <div className="cart-item">
+                <p>상품명</p>
+                <p>$40</p>
+                <p>1개</p>
+            </div>
         </div>
     )
 }
