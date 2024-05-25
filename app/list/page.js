078eef917e529cb1ea10f1dce74f68dba1c7593d
Cherisia <yhcyoon@kakao.com>
2024-05-25 17:40:11 +0900

    object, array 담긴 useState 바꾸기!!

--- app/list/page.js
+++ app/list/page.js
@@ -3,8 +3,11 @@
 import {useState} from "react";
 
 export default function List() {
-    let items = ['토마토', '파스타', '코코넛'];
-    let [count, setCount] = useState(0);
+    let [items, setItems] = useState([
+        {'name': '파스타', 'count': 0},
+        {'name': '피자', 'count': 0},
+        {'name': '치킨', 'count': 0}
+    ]);
     return (
         <div>
             <h2>Products</h2>
@@ -13,14 +16,18 @@ export default function List() {
                     return (
                         <div className="food" key={i}>
                             <img src={`/portfolio-${i + 1}.jpg`} className="food-img" alt="이미지"/>
-                            <h4>{item} $40</h4>
-                            <span> {count} </span>
+                            <h4>{item.name} $40</h4>
+                            <span> {item.count} </span>
                             <button onClick={() => {
-                                setCount(count + 1);
+                                let copyItems = [...items];
+                                copyItems[i].count++;
+                                setItems(copyItems);
                             }}>+
                             </button>
                             <button onClick={() => {
-                                setCount(count - 1);
+                                let copyItems = [...items];
+                                copyItems[i].count--;
+                                setItems(copyItems);
                             }}>-
                             </button>
                         </div>
