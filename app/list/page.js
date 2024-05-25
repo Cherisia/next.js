3926acf0d71c7a729cd9ac020f8c72227583ed8b
Cherisia <yhcyoon@kakao.com>
2024-05-25 17:12:04 +0900

    useState

--- app/list/page.js
+++ app/list/page.js
@@ -1,5 +1,10 @@
+'use client'
+
+import {useState} from "react";
+
 export default function List() {
     let items = ['토마토', '파스타', '코코넛'];
+    let [count, setCount] = useState(0);
     return (
         <div>
             <h2>Products</h2>
@@ -7,8 +12,17 @@ export default function List() {
                 items.map((item, i) => {
                     return (
                         <div className="food" key={i}>
-                            <img src={`/portfolio-${i+1}.jpg`} className="food-img" alt="이미지"/>
+                            <img src={`/portfolio-${i + 1}.jpg`} className="food-img" alt="이미지"/>
                             <h4>{item} $40</h4>
+                            <span> {count} </span>
+                            <button onClick={() => {
+                                setCount(count + 1);
+                            }}>+
+                            </button>
+                            <button onClick={() => {
+                                setCount(count - 1);
+                            }}>-
+                            </button>
                         </div>
                     );
                 })
